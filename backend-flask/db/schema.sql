43348c2a2b7722af8837ee24961148692dd12051
Shivam Gaonkar <shivamgaonkar2002@gmail.com>
2025-01-22 06:44:58 +0000

    some changes

--- backend-flask/db/schema.sql
+++ backend-flask/db/schema.sql
@@ -6,16 +6,16 @@ DROP TABLE IF EXISTS public.activities;
 CREATE TABLE public.users (
   uuid UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
   display_name text NOT NULL,
-  handle text NOT NULL,
-  email text NOT NULL,
-  cognito_user_id text NOT NULL,
+  handle text,
+  email text,
+  cognito_user_id text,
   created_at TIMESTAMP default current_timestamp NOT NULL
 );
 
 CREATE TABLE public.activities (
   uuid UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
   user_uuid UUID NOT NULL,
-  message text NOT NULL,
+  message text,
   replies_count integer DEFAULT 0,
   reposts_count integer DEFAULT 0,
   likes_count integer DEFAULT 0,
