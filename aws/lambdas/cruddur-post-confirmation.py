089f1ca4e2bc60d5bc70cec87112d8be7d5cfe5b
Shivam Gaonkar <shivamgaonkar2002@gmail.com>
2025-01-27 19:19:57 +0000

    sorta working

--- aws/lambdas/cruddur-post-confirmation.py
+++ aws/lambdas/cruddur-post-confirmation.py
@@ -11,35 +11,41 @@ def lambda_handler(event, context):
     user_email         = user['email']
     user_handle        = user['preferred_username']
     user_cognito_id    = user['sub']
+    
+    conn = None  # Initialize conn to None
+    
     try:
-      print('entered-try')
-      sql = f"""
-         INSERT INTO public.users (
-          display_name, 
-          email,
-          handle, 
-          cognito_user_id
-          ) 
-        VALUES(%s,%s,%s,%s)
-      """
-      print('SQL Statement ----')
-      print(sql)
-      conn = psycopg2.connect(os.getenv('CONNECTION_URL'))
-      cur = conn.cursor()
-      params = [
-        user_display_name,
-        user_email,
-        user_handle,
-        user_cognito_id
-      ]
-      cur.execute(sql,*params)
-      conn.commit() 
+        print('entered-try')
+        sql = f"""
+            INSERT INTO public.users (
+                display_name, 
+                email,
+                handle, 
+                cognito_user_id
+            ) 
+            VALUES(%s,%s,%s,%s)
+        """
+        print('SQL Statement ----')
+        print(sql)
+        
+        conn = psycopg2.connect(os.getenv('CONNECTION_URL'))  # Connect to the DB
+        cur = conn.cursor()
+        params = [
+            user_display_name,
+            user_email,
+            user_handle,
+            user_cognito_id
+        ]
+        cur.execute(sql, *params)
+        conn.commit()  # Commit changes to DB
 
     except (Exception, psycopg2.DatabaseError) as error:
-      print(error)
+        print(f"Error occurred: {error}")
+    
     finally:
-      if conn is not None:
-          cur.close()
-          conn.close()
-          print('Database connection closed.')
+        if conn is not None:
+            cur.close()
+            conn.close()  # Close the connection if it was successfully created
+            print('Database connection closed.')
+    
     return event
